8c7bd1f5096e4ec4ce9ab3d1746e4dc5a947f1e3
Martin Casas <casasmartinignacio@gmail.com>
2025-10-21 16:10:41 -0300

    initial commit

---
+++ app/drawings/page.tsx
@@ -0,0 +1,31 @@
+export default function DrawingsPage() {
+  return (
+    <div style={{
+      padding: '20px',
+      backgroundColor: '#ccffcc',
+      fontFamily: 'Courier New'
+    }}>
+      <h1 style={{
+        color: 'red',
+        fontSize: '45px',
+        textDecoration: 'line-through'
+      }}>
+        DRAWINGS PAGE
+      </h1>
+      <p style={{
+        color: 'blue',
+        fontSize: '16px',
+        lineHeight: '1.5'
+      }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
+      </p>
+      <p style={{
+        color: 'maroon',
+        fontSize: '16px',
+        lineHeight: '1.5'
+      }}>
+        Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.
+      </p>
+    </div>
+  );
+}
